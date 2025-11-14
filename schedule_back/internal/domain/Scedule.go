1500037d02e1c3d1f6383c8159501d68ee2dc7f7
mofius <antoon.s.ivanov@gmail.com>
2025-11-14 06:59:47 +0300

    Перенос всех сервисов в монореп + настройка docker-compose.yml

--- schedule_back/internal/domain/Scedule.go
+++
@@ -1,8 +0,0 @@
-package domain
-
-type Schedule struct {
-	ID           uint           `gorm:"primarykey" json:"id"`
-	Name         string         `gorm:"type:text;not null" json:"name"`
-	UniversityID uint           `gorm:"index" json:"university_id"`
-	Items        []ScheduleItem `gorm:"foreignKey:ScheduleID;constraint:OnDelete:CASCADE" json:"items"`
-}
--- schedule_back/internal/domain/user.go
+++
@@ -1,6 +0,0 @@
-package domain
-
-type User struct {
-	ID        uint       `gorm:"primaryKey;autoIncrement:false" json:"id"`
-	Schedules []Schedule `gorm:"many2many:user_schedules;" json:"schedules"`
-}
